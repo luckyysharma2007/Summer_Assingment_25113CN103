ce473397977c958ced0ce9353411f8a84a4b0ec9
luckyysharma2007 <luckyysharma.2007@gmail.com>
2026-06-03 23:22:26 +0530

    Update Ques2.c

--- Day1/Ques2.c
+++ Day1/Ques2.c
@@ -8,9 +8,10 @@ int main()
     printf("\nEnter a number: ");
     scanf("%d", &num);
 
+    printf("The multiplication table of %d is: \n", num);
+
     for (int i = 0; i <= 10; i++)
     {
-        printf("The multiplication table of %d is: \n", num);
         printf("%d x %d = %d\n", num, i, num * i);
     }
 
