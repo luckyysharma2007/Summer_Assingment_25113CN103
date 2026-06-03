76518fd86af36d0a8060bd79079cb008de46ca90
luckyysharma2007 <luckyysharma.2007@gmail.com>
2026-06-04 00:44:56 +0530

    Create Ques_15.c

---
+++ Day1 to 30/Ques_15.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, original, sum = 0, digit, n = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    int temp = num;
+
+    while (temp > 0)
+    {
+        temp = temp / 10;
+        n++;
+    }
+
+    temp = num;
+
+    while (temp > 0)
+    {
+        digit = temp % 10;
+        sum = sum + pow(digit, n);
+        temp = temp / 10;
+    }
+
+    if (sum == original)
+        printf("%d is an Armstrong number\n", original);
+    else
+        printf("%d is not an Armstrong number\n", original);
+
+        
+    return 0;
+}
